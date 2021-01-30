8e664b04adcb12f02d7b0b24eebc77289cc88f89
stelian2507 <76186047+stelian2507@users.noreply.github.com>
2021-01-30 15:22:53 -0500

    Add files via upload

---
+++ canvas-basics/javascripts/intro.js
@@ -0,0 +1,10 @@
+// javascripts/intro.js
+const canvas = document.getElementById('example');
+const ctx = canvas.getContext('2d');
+
+// ****************************
+// draw rectangle:
+// ****************************
+ 
+ctx.fillStyle = 'purple';
+ctx.fillRect(160, 160, 60, 60);
