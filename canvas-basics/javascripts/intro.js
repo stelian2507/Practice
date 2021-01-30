fb524dbce57601796b20354883f9ca314db2a373
Stelian Grosu <stelian.email@yahoo.com>
2021-01-30 16:06:02 -0500

    p2

--- canvas-basics/javascripts/intro.js
+++ canvas-basics/javascripts/intro.js
@@ -8,3 +8,27 @@ const ctx = canvas.getContext('2d');
  
 ctx.fillStyle = 'purple';
 ctx.fillRect(160, 160, 60, 60);
+
+// ****************************
+// draw path
+// ****************************
+
+// start the path
+ctx.beginPath();
+// starting position is x=50, y=50
+ctx.moveTo(50, 50);
+// draw the line that has final coordinates x=250, y=50
+ctx.lineTo(250, 50);
+
+// .stroke() executes the drawing
+ctx.stroke();
+
+// start a new line from these coordinates: x=250, y=50
+ctx.moveTo(250, 50);
+// draw the line that has final coordinates x=250, y=100
+ctx.lineTo(250, 100);
+// .stroke() executes the drawing
+ctx.stroke();
+
+// close the path
+ctx.closePath();
